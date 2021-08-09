94d5d3bdee6da4c1b1a05da352c4a5ec2176da3c
ZhengMin He <zhengmin099@gmail.com>
2021-08-09 23:31:33 +0800

    Update 20210809_01.sql

--- Interview/02/20210809_01.sql
+++ Interview/02/20210809_01.sql
@@ -1,23 +1,23 @@
 --有以下兩個資料表
 --order table
 -------------------------------------------------
---id	member	email	               date
---1	    ada	    ada@gmail.com	       2019-10-10
---2	    zoe	    zoe@travelskope.com	   2019-11-10
---3	    april	april@travelskope.com  2019-11-20
---4	    zoe	    zoe@travelskope.com	   2019-12-11
+--id	member	 email	                 date
+--1	    ada	   ada@gmail.com	         2019-10-10
+--2	    zoe	   zoe@travelskope.com	   2019-11-10
+--3	    april	 april@travelskope.com   2019-11-20
+--4	    zoe	   zoe@travelskope.com	   2019-12-11
 --其中 date 的資料型別為 varchar
 
 --orderItem table
 ------------------------------------
---orderId 	item	quantity  price
---1	        滑鼠	    1	      200
---1	        鍵盤	    1	      399
---2	        耳機	    1	      599
---2			螢幕		2	      4200
---3			surface	1		  49900
---3			繪圖板	1		  4000
---4			顯示卡	1	      5000
+--orderId 	item	   quantity  price
+--1	        滑鼠	   1	       200
+--1	        鍵盤	   1	       399
+--2	        耳機	   1	       599
+--2			    螢幕		 2	       4200
+--3			    surface  1		     49900
+--3			    繪圖板	  1		      4000
+--4			    顯示卡	  1	        5000
 --請寫出可獲得以下結果的 SQL：
 
 --1. 以訂單日期排序顯示所有訂單（不需列出訂單的購買項目）
