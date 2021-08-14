7efe8b5183c1fe02b30bad8f56f7b5c8bb359008
hezhengmin <zhengmin099@gmail.com>
2021-08-14 23:21:24 +0800

    not like

---
+++ sql-ex.ru/Ex35.sql
@@ -0,0 +1,8 @@
+-- Find models in the Product table consisting either of digits only or Latin letters (A-Z, case insensitive) only.
+-- Result set: model, type.
+
+select model ,type 
+from product
+where model not like '%[^0-9]%'
+or model not like '%[^a-z]%'
+or model not like '%[^A-Z]%'
