c9c978d7b2ba64aee599d0c1e2831a79e9c3562b
hezhengmin <zhengmin099@gmail.com>
2021-08-15 00:55:09 +0800

    intersect

---
+++ sql-ex.ru/Ex38.sql
@@ -0,0 +1,5 @@
+-- Find countries that ever had classes of both battleships (‘bb’) and cruisers (‘bc’).
+
+select country from classes where type ='bb'
+intersect
+select country from classes where type ='bc'
